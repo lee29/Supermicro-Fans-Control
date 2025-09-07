6310ba4482150edc6cb70fb60886a946c07b9cfa
lee29 <327208429@qq.com>
2025-09-07 19:32:59 +0800

    Update fcc.sh

--- fcc.sh
+++ fcc.sh
@@ -29,7 +29,7 @@ CONFIG_FILE="/root/ipmitool/config/config.cfg"
 main_menu() {
     while true; do
             # 输出欢迎消息
-        echo -e "${GREEN}欢迎使用 Dell-Fans-Control 一键部署脚本${PLAIN}"
+        echo -e "${GREEN}欢迎使用 Supermicro-Fans-Control 一键部署脚本${PLAIN}"
         echo -e "${GREEN}本程序将为您快速修改配置文件${PLAIN}"
         echo -e "${GREEN}如果您遇到问题可以在这里寻找答案： ${PLAIN}"
         echo -e "${YELLOW}https://github.com/AUKcl/Dell-Fans-Control${PLAIN}"
@@ -38,11 +38,11 @@ main_menu() {
         echo "1. 目标服务器IP地址"
         echo "2. IPMI用户名"
         echo "3. IPMI密码"
-        echo "4. 通知邮箱地址"
-        echo "5. SMTP服务器地址"
-        echo "6. SMTP服务器端口号"
-        echo "7. SMTP用户名"
-        echo "8. SMTP密码"
+#        echo "4. 通知邮箱地址"
+ #       echo "5. SMTP服务器地址"
+ #       echo "6. SMTP服务器端口号"
+  #      echo "7. SMTP用户名"
+   #     echo "8. SMTP密码"
         echo "9. 脚本运行超时参数"
         echo "10. 退出脚本"
 
@@ -58,21 +58,21 @@ main_menu() {
             3)
                 update_password
                 ;;
-            4)
-                update_email
-                ;;
-            5)
-                update_smtp_server
-                ;;
-            6)
-                update_smtp_port
-                ;;
-            7)
-                update_smtp_username
-                ;;
-            8)
-                update_smtp_password
-                ;;
+            # 4)
+            #     update_email
+            #     ;;
+            # 5)
+            #     update_smtp_server
+            #     ;;
+            # 6)
+            #     update_smtp_port
+            #     ;;
+            # 7)
+            #     update_smtp_username
+            #     ;;
+            # 8)
+            #     update_smtp_password
+            #     ;;
             9)
                 update_timeout
                 ;;
@@ -170,115 +170,115 @@ update_password() {
     done
 }
 
-update_email() {
-    while true; do
-        echo "1. 请输入新的通知邮箱地址"
-        echo "2. 返回上一级"
-        read -p "请输入选项 (1-2): " choice
+# update_email() {
+#     while true; do
+#         echo "1. 请输入新的通知邮箱地址"
+#         echo "2. 返回上一级"
+#         read -p "请输入选项 (1-2): " choice
 
-        case $choice in
-            1)
-                read -p "请输入新的通知邮箱地址: " new_email
-                sed -i "s/EMAIL=.*/EMAIL=$new_email/" $CONFIG_FILE
-                echo "已更新通知邮箱地址为: $new_email"
-                ;;
-            2)
-                return
-                ;;
-            *)
-                echo "无效的选项"
-                ;;
-        esac
-    done
-}
+#         case $choice in
+#             1)
+#                 read -p "请输入新的通知邮箱地址: " new_email
+#                 sed -i "s/EMAIL=.*/EMAIL=$new_email/" $CONFIG_FILE
+#                 echo "已更新通知邮箱地址为: $new_email"
+#                 ;;
+#             2)
+#                 return
+#                 ;;
+#             *)
+#                 echo "无效的选项"
+#                 ;;
+#         esac
+#     done
+# }
 
-update_smtp_server() {
-    while true; do
-        echo "1. 请输入新的SMTP服务器地址"
-        echo "2. 返回上一级"
-        read -p "请输入选项 (1-2): " choice
+# update_smtp_server() {
+#     while true; do
+#         echo "1. 请输入新的SMTP服务器地址"
+#         echo "2. 返回上一级"
+#         read -p "请输入选项 (1-2): " choice
 
-        case $choice in
-            1)
-                read -p "请输入新的SMTP服务器地址: " new_smtp_server
-                sed -i "s/SMTP_SERVER=.*/SMTP_SERVER=$new_smtp_server/" $CONFIG_FILE
-                echo "已更新SMTP服务器地址为: $new_smtp_server"
-                ;;
-            2)
-                return
-                ;;
-            *)
-                echo "无效的选项"
-                ;;
-        esac
-    done
-}
+#         case $choice in
+#             1)
+#                 read -p "请输入新的SMTP服务器地址: " new_smtp_server
+#                 sed -i "s/SMTP_SERVER=.*/SMTP_SERVER=$new_smtp_server/" $CONFIG_FILE
+#                 echo "已更新SMTP服务器地址为: $new_smtp_server"
+#                 ;;
+#             2)
+#                 return
+#                 ;;
+#             *)
+#                 echo "无效的选项"
+#                 ;;
+#         esac
+#     done
+# }
 
-update_smtp_port() {
-    while true; do
-        echo "1. 请输入新的SMTP服务器端口号"
-        echo "2. 返回上一级"
-        read -p "请输入选项 (1-2): " choice
+# update_smtp_port() {
+#     while true; do
+#         echo "1. 请输入新的SMTP服务器端口号"
+#         echo "2. 返回上一级"
+#         read -p "请输入选项 (1-2): " choice
 
-        case $choice in
-            1)
-                read -p "请输入新的SMTP服务器端口号: " new_smtp_port
-                sed -i "s/SMTP_PORT=.*/SMTP_PORT=$new_smtp_port/" $CONFIG_FILE
-                echo "已更新SMTP服务器端口号为: $new_smtp_port"
-                ;;
-            2)
-                return
-                ;;
-            *)
-                echo "无效的选项"
-                ;;
-        esac
-    done
-}
+#         case $choice in
+#             1)
+#                 read -p "请输入新的SMTP服务器端口号: " new_smtp_port
+#                 sed -i "s/SMTP_PORT=.*/SMTP_PORT=$new_smtp_port/" $CONFIG_FILE
+#                 echo "已更新SMTP服务器端口号为: $new_smtp_port"
+#                 ;;
+#             2)
+#                 return
+#                 ;;
+#             *)
+#                 echo "无效的选项"
+#                 ;;
+#         esac
+#     done
+# }
 
-update_smtp_username() {
-    while true; do
-        echo "1. 请输入新的SMTP用户名"
-        echo "2. 返回上一级"
-        read -p "请输入选项 (1-2): " choice
+# update_smtp_username() {
+#     while true; do
+#         echo "1. 请输入新的SMTP用户名"
+#         echo "2. 返回上一级"
+#         read -p "请输入选项 (1-2): " choice
 
-        case $choice in
-            1)
-                read -p "请输入新的SMTP用户名: " new_smtp_username
-                sed -i "s/SMTP_USERNAME=.*/SMTP_USERNAME=$new_smtp_username/" $CONFIG_FILE
-                echo "已更新SMTP用户名为: $new_smtp_username"
-                ;;
-            2)
-                return
-                ;;
-            *)
-                echo "无效的选项"
-                ;;
-        esac
-    done
-}
+#         case $choice in
+#             1)
+#                 read -p "请输入新的SMTP用户名: " new_smtp_username
+#                 sed -i "s/SMTP_USERNAME=.*/SMTP_USERNAME=$new_smtp_username/" $CONFIG_FILE
+#                 echo "已更新SMTP用户名为: $new_smtp_username"
+#                 ;;
+#             2)
+#                 return
+#                 ;;
+#             *)
+#                 echo "无效的选项"
+#                 ;;
+#         esac
+#     done
+# }
 
-update_smtp_password() {
-    while true; do
-        echo "1. 请输入新的SMTP密码"
-        echo "2. 返回上一级"
-        read -p "请输入选项 (1-2): " choice
+# update_smtp_password() {
+#     while true; do
+#         echo "1. 请输入新的SMTP密码"
+#         echo "2. 返回上一级"
+#         read -p "请输入选项 (1-2): " choice
 
-        case $choice in
-            1)
-                read -p "请输入新的SMTP密码: " new_smtp_password
-                sed -i "s/SMTP_PASSWORD=.*/SMTP_PASSWORD=$new_smtp_password/" $CONFIG_FILE
-                echo "已更新SMTP密码"
-                ;;
-            2)
-                return
-                ;;
-            *)
-                echo "无效的选项"
-                ;;
-        esac
-    done
-}
+#         case $choice in
+#             1)
+#                 read -p "请输入新的SMTP密码: " new_smtp_password
+#                 sed -i "s/SMTP_PASSWORD=.*/SMTP_PASSWORD=$new_smtp_password/" $CONFIG_FILE
+#                 echo "已更新SMTP密码"
+#                 ;;
+#             2)
+#                 return
+#                 ;;
+#             *)
+#                 echo "无效的选项"
+#                 ;;
+#         esac
+#     done
+# }
 
 update_timeout() {
     while true; do
